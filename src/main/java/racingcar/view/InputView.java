5e9f53d9b1732aaabd62294f999ab0c04b49e616
류연경 <ykr.dev@gmail.com>
2025-10-27 12:44:47 +0900

    feat: 값 누락 예외 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
 
     public static final String INPUT_INFORMATION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_TRY_NUM_INFORMATION = "시도할 횟수는 몇 회인가요?";
+    public static final String INPUT_DELIMITER = ",";
 
     private InputView() {
     }
@@ -23,6 +24,7 @@ public class InputView {
         printInputInfo();
         String input = userInput();
         validateDelimiter(input);
+        validateValueMissing(input);
         return parseInputToCars(input);
     }
 
@@ -37,16 +39,21 @@ public class InputView {
         }
     }
 
-    // ??
     public Cars parseInputToCars(String input) {
-        List<Car> cars = Arrays.stream(input.split(","))
+        List<Car> cars = Arrays.stream(input.split(INPUT_DELIMITER))
                 .map(Car::of).toList();
         return new Cars(cars);
     }
 
     private void validateDelimiter(String input) {
-        if (!input.contains(",")) {
+        if (!input.contains(INPUT_DELIMITER)) {
             throw new IllegalArgumentException(ErrorMessage.EXCEPTION_ONLY_ONE_CAR.getMessage());
+        }
+    }
+
+    private void validateValueMissing(String input) {
+        if (input.endsWith(INPUT_DELIMITER)) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEPTION_VALUE_MISSING.getMessage());
         }
     }
 
